0c40be9c8d9f756e3914a7283fabd974a7947e38
varbanov88 <varbanov_88@abv.bg>
2017-05-24 17:13:06 +0300

    Stacks & Queues all lab tasks solved

---
+++ StacksAndQueues/HotPotato/HotPotato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var queue = new Queue<string>(input);
+            var num = int.Parse(Console.ReadLine());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 0; i < num - 1; i++)
+                {
+                    string remainer = queue.Dequeue();
+                    queue.Enqueue(remainer);
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
