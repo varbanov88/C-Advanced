6486540a90c7553184678df3c405a89b165c8322
varbanov88 <varbanov_88@abv.bg>
2017-05-24 16:23:03 +0300

    Stack tasks solved

---
+++ StacksAndQueues/ReverseString/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseString
+{
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StacksAndQueues/SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var remainer = input.Split();
+
+            var stack = new Stack<string>(remainer.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNumber = int.Parse(stack.Pop());
+                var operant = stack.Pop();
+                var secondNumber = int.Parse(stack.Pop());
+
+                if (operant == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+
+                else
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
