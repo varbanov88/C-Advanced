aa7dfc6d655216e649b98339a216fa5f900fe95f
varbanov88 <varbanov_88@abv.bg>
2017-05-31 20:44:45 +0300

    2 exercises tasks solved

---
+++ Matrices/DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    public class DiagonalDifference
+    {
+        public static void Main()
+        {
+            var height = int.Parse(Console.ReadLine());
+
+            long[][] matrix = new long[height][];
+            long leftDiagonalSum = 0;
+            long rightDiagonalSum = 0;
+
+            for (int i = 0; i < height; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                                    .Split()
+                                    .Select(long.Parse)
+                                    .ToArray();
+
+                leftDiagonalSum += matrix[i][i];
+                rightDiagonalSum += matrix[i][matrix[i].Length - 1 - i];
+
+            }
+
+            Console.WriteLine(Math.Abs(leftDiagonalSum - rightDiagonalSum));
+        }
+    }
+}
---
+++ Matrices/MatrixOfPalindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace MatrixOfPalindromes
+{
+    public class MatrixOfPalindromes
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            string[][] matrix = new string[input[0]][];
+
+            for (int row = 0; row < input[0]; row++)
+            {
+                matrix[row] = new string[input[1]];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    var pol = new StringBuilder();
+                    pol.Append(alphabet[row]);
+                    pol.Append(alphabet[row + col]);
+                    pol.Append(alphabet[row]);
+
+                    matrix[row][col] = pol.ToString();
+                }
+            }
+
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
