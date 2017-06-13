cc096d31355edffcb51e6d41ad89c5b19b3fe01d
varbanov88 <varbanov_88@abv.bg>
2017-06-13 20:49:35 +0300

    Regex exercises solved

---
+++ Regex/SemanticHTML/SemanticHTML.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SemanticHTML
+{
+    public class SemanticHTML
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+
+
+            while (line != "END")
+            {
+                var openingMatch = Regex.Match(line, @"<(div)([^>]+)(?:id|class)\s*=\s*""(.*?)""(.*?)>");
+                var closingMatch = Regex.Match(line, @"<\/div>\s*<!--\s*(.*?)\s*-->");
+
+                if (openingMatch.Success)
+                {
+                    line = Regex.Replace(line, @"<(div)([^>]+)(?:id|class)\s*=\s*""(.*?)""(.*?)>", @"$3 $2 $4").Trim();
+                    line = "<" + Regex.Replace(line, @"\s+", " ") + ">";
+                }
+
+                else if (closingMatch.Success)
+                {
+                    line = "</" + closingMatch.Groups[1] + ">";
+                }
+
+                Console.WriteLine(line);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
--- Regex/UseYourChainsBuddy/UseYourChainsBuddy.cs
+++ Regex/UseYourChainsBuddy/UseYourChainsBuddy.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace UseYourChainsBuddy
 {
@@ -11,78 +8,38 @@ namespace UseYourChainsBuddy
     {
         public static void Main()
         {
-            //not finished
-            var pattern = new Regex("<p>(.+?)<\\/p>");
+            var regex = new Regex("<p>(.+?)<\\/p>");
             string input = Console.ReadLine();
+            var matches = regex.Matches(input);
 
             var sb = new StringBuilder();
 
-            var matches = pattern.Matches(input);
-
             foreach (Match match in matches)
             {
-                var line = match.Groups[1].Value;
-                line = Regex.Replace(line, "\\s+", " ");
-                sb.Append(line).Append(" ");
-            }
+                var whiteSpaces = "[^a-z0-9]+";
+                var remainder = match.Groups[1].Value;
+                var replaced = Regex.Replace(remainder, whiteSpaces, " ");
 
-            var textToReplace = Regex.Replace(sb.ToString(), "[^a-z0-9]+", " ");
-            var output = new StringBuilder();
-
-            foreach (var ch in textToReplace)
-            {
-                if (Char.IsUpper(ch))
+                foreach (var ch in replaced)
                 {
-                    output.Append(ch);
-                }
-
-                else if (ch == ' ')
-                {
-                    output.Append(ch);
-                }
-
-                else if (Char.IsDigit(ch))
-                {
-                    output.Append(ch);
-                }
-
-                else
-                {
-                    switch (ch)
+                    if (ch >= 'a' && ch <= 'm')
                     {
-                        case 'a': output.Append('n'); break;
-                        case 'b': output.Append('o'); break;
-                        case 'c': output.Append('p'); break;
-                        case 'd': output.Append('q'); break;
-                        case 'e': output.Append('r'); break;
-                        case 'f': output.Append('s'); break;
-                        case 'g': output.Append('t'); break;
-                        case 'h': output.Append('u'); break;
-                        case 'i': output.Append('v'); break;
-                        case 'j': output.Append('w'); break;
-                        case 'k': output.Append('x'); break;
-                        case 'l': output.Append('y'); break;
-                        case 'm': output.Append('z'); break;
-                        case 'n': output.Append('a'); break;
-                        case 'o': output.Append('b'); break;
-                        case 'p': output.Append('c'); break;
-                        case 'q': output.Append('d'); break;
-                        case 'r': output.Append('e'); break;
-                        case 's': output.Append('f'); break;
-                        case 't': output.Append('g'); break;
-                        case 'u': output.Append('h'); break;
-                        case 'v': output.Append('i'); break;
-                        case 'w': output.Append('j'); break;
-                        case 'x': output.Append('k'); break;
-                        case 'y': output.Append('l'); break;
-                        case 'z': output.Append('m'); break;
+                        sb.Append((char)(ch + 13));
+                    }
+
+                    else if (ch >= 'n' && ch <= 'z')
+                    {
+                        sb.Append((char)(ch - 13));
+                    }
+
+                    else
+                    {
+                        sb.Append(ch);
                     }
                 }
             }
+            Console.WriteLine(sb);
 
-            var result = Regex.Replace(output.ToString(), "\\s+", " ");
-
-            Console.WriteLine(result);
         }
     }
 }
